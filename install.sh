f9e2efd877684944c1979f8be94b4f559d50ca2e
ferdinand12345 <ferdshinodas@gmail.com>
2019-11-13 00:33:10 +0700

    Create installation script

--- install.sh
+++ install.sh
@@ -4,5 +4,11 @@
 echo "Laravel Project Installation:";
 echo "-------------------------------------------";
 
-# 1. Install Vendor
+# 1. Install composer.json via Composer
 composer install
+
+# 2. Change mode folder ./vendor ./bootstrap ./database & ./storage to 777 (Recursive)
+chmod -R 777 ./vendor ./bootstrap ./database ./storage
+
+# 3. Run Test
+./vendor/bin/phpunit
