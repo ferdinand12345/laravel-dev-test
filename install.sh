765a84a5b23193cd0160832c8b8249d97f5cd6e2
ferdinand12345 <ferdshinodas@gmail.com>
2019-11-13 00:55:39 +0700

    Create installation script

--- install.sh
+++ install.sh
@@ -2,24 +2,30 @@
 
 # Laravel Project Installation
 echo "Laravel Project Installation:";
-echo "-------------------------------------------";
 
 # 1. Install composer.json via Composer
 composer install
 
+# 2. Move env to .env
 mv env .env
 
+# 3. Generate Laravel Key
 php artisan key:generate
+
+# 4. Clear Cache
 php artisan cache:clear
+
+# 5. Clear Config
 php artisan config:clear
+
+# 6. Dump Composer Autoload
 composer dump-autoload
 
-# 2. Change mode folder ./vendor ./bootstrap ./database & ./storage to 777 (Recursive)
+# 7. Change mode folder ./vendor ./bootstrap ./database & ./storage to 777 (Recursive)
 chmod -R 777 ./vendor ./bootstrap ./database ./storage
 
-# 3. Run Test
+# 8. Run Test
 ./vendor/bin/phpunit
 
+# 9. Run Laravel on http://127.0.0.1:8000
 php artisan serve
-
-xdg-open https://stackoverflow.com
