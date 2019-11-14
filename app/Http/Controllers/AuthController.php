75520ddc8067558f8753e9afc2155dc3d8d258b4
verdomazeg <bertaruh.rindoe@gmail.com>
2019-11-14 18:32:48 +0700

    Set routing

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,97 +20,6 @@ class AuthController extends Controller {
 		}
 	}
 
-	/*
-	public function register_form() {
-		return view( 'auth/register-form' );
-	}
-
-	public function register_process( Request $request ) {
-		# Setup Validation
-		$validator = Validator::make( $request->all(), [
-			'NAME' => 'required|max:64',
-			'SURNAME' => 'max:64',
-			'EMAIL' => 'required|email|max:320',
-			'PASSWORD' => 'required|min:6|required_with:PASSWORD_CONF|same:PASSWORD_CONF',
-			'PASSWORD_CONF' => 'min:6',
-			'PHONE_NUMBER' => 'required'
-		] );
-
-		# Setup Custom Validation - Check Email Address
-		$check_email = self::check_email( $request->input( 'EMAIL' ) );
-		$validator->after( function( $validator ) use ( $check_email ) {
-			if ( $check_email->COUNT > 0 ) {
-				$validator->errors()->add( 'EMAIL', 'The email is not available.');
-			}
-		} );
-
-		# Run Validation
-		if ( $validator->fails() ) {
-			return redirect( 'register' )->withErrors( $validator )->withInput();
-		}
-		else {
-			$IN_NAME = addslashes( $request->input( 'NAME' ) );
-			$IN_SURNAME = addslashes( $request->input( 'SURNAME' ) );
-			$IN_PHONE_NUMBER = addslashes( $request->input( 'PHONE_NUMBER' ) );
-			$IN_EMAIL = addslashes( $request->input( 'EMAIL' ) );
-			$IN_DOB = date( 'Y-m-d H:i:s', strtotime( addslashes( $request->input( 'DOB' ) ) ) );
-			$IN_PASSWORD = md5( addslashes( $request->input( 'PASSWORD' ) ) );
-			$sql_statement = ( "
-				INSERT INTO 
-					TM_USER(
-						ID, 
-						NAME, 
-						SURNAME, 
-						DOB, 
-						EMAIL, 
-						PASSWORD, 
-						PHONE_NUMBER,
-						ADDRESS,
-						COUNTRY,
-						TRADING_ACCOUNT_NUMBER,
-						BALANCE,
-						OPEN_TRADES,
-						CLOSE_TRADES
-					) 
-				VALUES (
-					NULL, 
-					'{$IN_NAME}', 
-					'{$IN_SURNAME}', 
-					'{$IN_DOB}', 
-					'{$IN_EMAIL}', 
-					'{$IN_PASSWORD}',
-					'{$IN_PHONE_NUMBER}',
-					'10 Eunos Road 8, #05-33 Singapore Post Centre',
-					'SG',
-					'IN01234567891044',
-					-100.22,
-					211.299,
-					211.299
-				)
-			" );
-
-			try {
-				$run_query = DB::insert( $sql_statement );
-				if ( $run_query == true ) {
-					$set_login = $request->session()->put( [
-						'LOGIN_DATA' => array(
-							'ID' => DB::getPdo()->lastInsertId()
-						)
-					] );
-					if ( !$set_login ) {
-						return redirect( 'dashboard' );
-					}
-					else {
-						return redirect( 'register' )->withInput();
-					}
-				}
-			} 
-			catch( \Illuminate\Database\QueryException $exception ) { 
-				return abort( 500 );
-			}
-		}
-	}
-
 	public function login_form() {
 		return view( 'auth/login-form' );
 	}
@@ -139,12 +48,17 @@ class AuthController extends Controller {
 			$IN_PASSWORD = md5( addslashes( $request->input( 'PASSWORD' ) ) );
 			$sql_statement = ( "
 				SELECT 
-					ID
+					A.ID,
+					A.EMAIL,
+					A.ROLE_ID,
+					B.NAME AS ROLE_NAME
 				FROM 
-					TM_USER
+					TM_USER A
+					INNER JOIN TM_ROLE B
+						ON A.ROLE_ID = B.ID
 				WHERE 
-					EMAIL = '{$IN_EMAIL}' 
-					AND PASSWORD = '{$IN_PASSWORD}'
+					A.EMAIL = '{$IN_EMAIL}' 
+					AND A.PASSWORD = '{$IN_PASSWORD}'
 				LIMIT 1
 			" );
 
@@ -153,7 +67,10 @@ class AuthController extends Controller {
 			if ( !empty( $run_query ) ) {
 				$set_login = $request->session()->put( [
 					'LOGIN_DATA' => array(
-						'ID' => $run_query->ID
+						'ID' => $run_query->ID,
+						'ROLE_ID' => $run_query->ROLE_ID,
+						'ROLE_NAME' => $run_query->ROLE_NAME,
+						'EMAIL' => $run_query->EMAIL
 					)
 				] );
 				if ( !$set_login ) {
@@ -175,6 +92,13 @@ class AuthController extends Controller {
 		return redirect( 'login' );
 	}
 
+	public function check_session() {
+		$data = session()->all();
+		print '<pre>';
+		print_r($data);
+		print '</pre>';
+	}
+
 	private function check_email( $email, $password = '' ) {
 		# Get TRUE/FALSE from TM_USER by Email Address
 		$email = addslashes( $email );
@@ -189,67 +113,6 @@ class AuthController extends Controller {
 				{$password}
 		" ) )->first();
 		return $query;
-	}
-	*/
-
-	public function login_process( Request $request ) {
-		// $IN_EMAIL = addslashes( $request->input( 'EMAIL' ) );
-		// $IN_PASSWORD = md5( addslashes( $request->input( 'PASSWORD' ) ) );
-		$IN_EMAIL = 'admin@email.com';
-		$IN_PASSWORD = md5( '1234567890' );
-		$sql_statement = ( "
-			SELECT 
-				A.ID,
-				A.EMAIL,
-				A.ROLE_ID,
-				B.NAME AS ROLE_NAME
-			FROM 
-				TM_USER A
-				INNER JOIN TM_ROLE B
-					ON A.ROLE_ID = B.ID
-			WHERE 
-				A.EMAIL = '{$IN_EMAIL}' 
-				AND A.PASSWORD = '{$IN_PASSWORD}'
-			LIMIT 1
-		" );
-
-		$run_query = collect( \DB::select( $sql_statement ) )->first();
-
-		if ( !empty( $run_query ) ) {
-			$set_login = $request->session()->put( [
-				'LOGIN_DATA' => array(
-					'ID' => $run_query->ID,
-					'ROLE_ID' => $run_query->ROLE_ID,
-					'ROLE_NAME' => $run_query->ROLE_NAME,
-					'EMAIL' => $run_query->EMAIL
-				)
-			] );
-			if ( !$set_login ) {
-				print 'OK';
-				// return redirect( 'dashboard' );
-			}
-			else {
-				print 'Gak OK';
-				// return redirect( 'login' )->withInput();
-			}
-		}
-		else {
-			print 'Internal Server Error';
-			// return abort( 500 );
-		}
-	}
-
-	public function logout_process() {
-		session()->flush();
-		// return redirect( 'login' );
-		print 'Done';
-	}
-
-	public function check_session() {
-		$data = session()->all();
-		print '<pre>';
-		print_r($data);
-		print '</pre>';
 	}
 
 }
--- routes/web.php
+++ routes/web.php
@@ -10,26 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Route::get( '/login', 'AuthController@login_form' );
-// Route::post( '/login', 'AuthController@login_process' );
-// Route::get( '/register', 'AuthController@register_form' );
-// Route::post( '/register', 'AuthController@register_process' );
-// Route::group( [ 'middleware' => 'in_session' ], function() {
-// 	Route::get( '/', 'DashboardController@index' );
-// 	Route::get( '/dashboard', 'DashboardController@index' );
-// 	Route::get( '/logout', 'AuthController@logout_process' );
-
-// } );
+Route::get( '/login', 'AuthController@login_form' );
+Route::post( '/login', 'AuthController@login_process' );
+Route::group( [ 'middleware' => 'in_session' ], function() {
+	Route::get( '/', 'ClientController@index' );
+	Route::get( '/dashboard', 'ClientController@index' );
+	Route::get( '/create-user', 'UserController@user_create_form' );
+	Route::post( '/create-user', 'UserController@user_create_process' );
+	Route::get( '/user', 'UserController@index' );
+	Route::get( '/logout', 'AuthController@logout_process' );
+} );
 
 Route::get( '/cs', 'AuthController@check_session' );
-// Route::get( '/testing', 'AuthController@testing' );
-Route::get( '/', 'AuthController@login_process' );
-Route::get( '/login', 'AuthController@login_process' );
-Route::get( '/logout', 'AuthController@logout_process' );
-
-Route::get( '/create-user', 'UserController@user_create_form' );
-Route::post( '/create-user', 'UserController@user_create_process' );
-Route::get( '/user', 'UserController@index' );
-
-Route::get( '/', 'ClientController@index' );
-Route::get( '/dashboard', 'ClientController@index' );
